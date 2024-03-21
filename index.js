bb2e679910a5cb46e5cda2ad6da536e459545ace
Michael Roche <mikeroche138@gmail.com>
2024-03-21 12:20:32 +0300

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
 // Write your solution in this file!
+const employee = {
+    name: "Migero Mike",
+    streetAddress: "Ngong Lane"
+  };
+
+function updateEmployeeWithKeyAndValue(employee, key, value){
+    return {...employee, [key] : value};  
+}
+
+function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value){
+    employee[key] = value;
+    return employee;
+}
+
+function deleteFromEmployeeByKey(employee, key){
+    return {...employee, [key]: undefined};
+}
+
+function destructivelyDeleteFromEmployeeByKey(employee, key){
+    delete employee[key];
+    return employee;
+}
